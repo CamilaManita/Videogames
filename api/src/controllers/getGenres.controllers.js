9dfd3204b3bdb1536b0a7263266255eaddcd2246
Camila Manita <camilamanita3@gmail.com>
2023-09-15 12:03:12 -0300

    Hago controllers

---
+++ api/src/controllers/getGenres.controllers.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const { Genre } = require ('../db');
+const { API_GENRES, API_KEY } = process.env;
+
+const getGenres = async () => {
+    const response = await axios.get(`${API_GENRES}?${API_KEY}`)
+
+    const genresAPI = response.data.results;
+
+    genresAPI.forEach((genre) => {
+        Genre.findOrCreate({
+            where: {
+                name: genre.name
+            }
+        })
+    })
+    return 'genresCreated';
+}
+
+module.exports = getGenres;
---
+++ api/src/controllers/getVideogames.controllers.js
@@ -0,0 +1,42 @@
+const { Videogames } = require('../db');
+const { API_URL, API_KEY } = process.env;
+
+const getAllGames = async () => {
+  const dbVGames = await Videogames.findAll({
+    attributes: ["id", "name", "image", "rating"],
+    include: [
+      {
+        model: Genre,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+  const urls = [];
+  for (let i = 1; i <= 5; i++) {
+    urls.push(axios.get(`${API_URL}?${API_KEY}&page=${i}`));
+  }
+  const axiosAllResponses = await Promise.all(urls);
+  const apiVGames = axiosAllResponses
+    .map((r) => r.data.results)
+    .flat()
+    .map((vg) => {
+      return {
+        id: vg.id,
+        name: vg.name,
+        image: vg.background_image,
+        rating: vg.rating,
+        genres: vg.genres.map((g) => {
+          return { name: g.name };
+        }),
+      };
+    });
+  const allGames = [...dbVGames, ...apiVGames].slice(0, 100);
+  console.log(allGames.length);
+
+  return allGames;
+};
+
+module.exports = getAllGames;
