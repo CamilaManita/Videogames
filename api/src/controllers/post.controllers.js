2b773026a4008c513bf944135abeaf1e6998377b
Camila Manita <camilamanita3@gmail.com>
2023-09-19 11:46:35 -0300

    Arreglo post videogame pero todavia no es funcional

--- api/src/controllers/post.controllers.js
+++ api/src/controllers/post.controllers.js
@@ -1,23 +1,23 @@
 const { Videogame } = require('../db');
 
-const postVideogame = async (
-  name,
-  description,
-  platforms,
-  image,
-  releaseDate,
-  rating,
-  genres
-) => {
-  return await Videogame.create({
-    name,
-    description,
-    platforms,
-    image,
-    releaseDate,
-    rating,
-    genres,
+const postVideogame = async ( name, description, platforms, image, releaseDate, rating, genres ) => {
+  if ( !name || !description || !platforms || !image || !releaseDate || !rating || !genres )
+
+  throw new Error('There is not all the required information');
+
+  const existVideogame = await Videogame.findOne({
+    where: { name: name }
   });
+
+  if (existVideogame) throw new Error("The name of this videogame already exists, please try another name");
+
+  const newVideogame = await Videogame.create({
+    name, description, platforms, image, releaseDate, rating
+  });
+
+  await newVideogame.addGenres(genres);
+
+  return newVideogame;
 }
 
 module.exports = postVideogame;
