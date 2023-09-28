4a3f1a657f12ec75e1505ff1caabde4a91ab7887
Camila Manita <camilamanita3@gmail.com>
2023-09-28 10:54:25 -0300

    userRouter created

--- api/src/routes/userRouter.js
+++ api/src/routes/userRouter.js
@@ -1,3 +1,9 @@
 const { Router } = require('express');
+const { userLogin, userRegister } = require('../handlers/userHandler');
 
 const userRouter = Router();
+
+userRouter.post('/login', userLogin);
+userRouter.post('/register', userRegister);
+
+module.exports = userRouter;
