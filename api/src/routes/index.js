69376ef659e813f92d64a7a39c8ba2ace49c8960
Camila Manita <camilamanita3@gmail.com>
2023-09-28 10:56:23 -0300

    Agrego userRouter a index de routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const gameRouter = require('./videogameRouter');
 const genreRouter = require('./genreRouter');
+const userRouter = require('./userRouter');
 
 const router = Router();
 
 router.use("/games", gameRouter);
 router.use("/genres", genreRouter);
+router.use('/user', userRouter);
 
 module.exports = router;
