e57275e720f67d983faa2bf6b8a598aeaaf63d50
Camila Manita <camilamanita3@gmail.com>
2023-09-28 10:51:13 -0300

    handler userLogin created

--- api/src/handlers/userHandler.js
+++ api/src/handlers/userHandler.js
@@ -56,3 +56,28 @@ const userRegister = async (req,res) => {
         return res.status(error.errors[0].type).json({ error: error.message });
     }
 }
+
+const userLogin = async (req,res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ error: 'username and password are required'});
+        }
+
+        const user = await User.findOne({ where: {email: email} });
+
+        if (!user) {
+            return res.status(404).json({ error: 'user not found' });
+        }
+
+        if (user.password !== password) {
+            return res.status(403).json({ error: 'wrong password'});
+        }
+
+        return res.status(200).json({ access: true, message: 'welcome back!'})
+    } catch (error) {
+        return res.status(error.errors[0].type).json({ error: error.message});
+    }
+}
+
+module.exports = { createUser, userRegister, userLogin}
