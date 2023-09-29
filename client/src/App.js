61a3b1a221ff76c31b0ae16e76327216a1f65e59
Camila Manita <camilamanita3@gmail.com>
2023-09-29 12:29:03 -0300

    Creo rutas para las views

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
-// import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Home, Landing, Detail, Form } from './Views';
 
 function App() {
   // const location = useLocation();
@@ -8,6 +9,12 @@ function App() {
   return (
     <div className="App">
       <h1>Videogames</h1>
+      <Routes>
+        <Route exact path='/' element={<Landing />} />
+        <Route path='/home' element={<Home />} />
+        <Route exact path='/detail/:id' element={<Detail />} />
+        <Route exact path='/create' element={<Form/>} />
+      </Routes>
     </div>
   );
 }
