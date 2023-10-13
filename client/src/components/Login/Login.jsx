cac0e65764f4a1c2242cda92279c0371dea49183
Camila Manita <camilamanita3@gmail.com>
2023-10-12 23:09:52 -0300

    login sketch created

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { loginValidation } from "../../Utils/Validations";
 
-function Login() {
+function Login({ login, response, setResponse }) {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    setUserData({ ...userData, [event.target.name]: event.target.value });
+    setErrors(
+      loginValidation({ ...userData, [event.target.name]: event.target.value })
+    );
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    login(userData);
+  };
+
+  useEffect(() => {
+    setResponse("");
+  }, []);
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div>
+          <div>
+            <label htmlFor="email"> Email</label>
+          </div>
+
+          <div>
+            <input
+              type="email"
+              value={userData.email}
+              placeholder="Your Email"
+              onChange={handleChange}
+              name="email"
+            />
+            {errors.email && <p>{errors.email}</p>}
+          </div>
+        </div>
+        <div>
+          <div>
+            <label htmlFor="password">Password</label>
+          </div>
+
+          <div>
+            <input
+              type="password"
+              value={userData.password}
+              placeholder="Your Password"
+              onChange={handleChange}
+              name="password"
+            />
+            {errors.password && <p>{errors.password}</p>}
+          </div>
+        </div>
+
+        <button>Enter</button>
+      </form>
+      {response && (
+        <div className="">
+          <h2>{response}</h2>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Login
+export default Login;
