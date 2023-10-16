534206cceed934bae626bcd9e7b579d5cf7378e9
Camila Manita <camilamanita3@gmail.com>
2023-10-15 22:41:12 -0300

    first landing view created

--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
+import Login from '../../components/Login/Login';
+import Register from '../../components/Register/Register';
 
-function Landing() {
+function Landing({ login, response, setResponse}) {
+  const [openRegister, setOpenRegister] = useState(false);
+
   return (
-    <div>Landing</div>
+    <div>
+      <div>
+        {!openRegister && (
+          <Login login={login} response={response} setResponse={setResponse} />
+        )}
+
+        {!openRegister && (
+          <button onClick={() => setOpenRegister(true)}>
+            Sing Up
+          </button>
+        )}
+
+        <div>
+          {openRegister && (
+            <Register setOpenRegister={setOpenRegister} login={login} />
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
